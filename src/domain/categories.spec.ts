bc91f228f9bf0468d672a6042e86d36d45d452c0
Den <ekisde.xp@gmail.com>
2025-06-21 13:22:59 +0200

    docs(test): improve test docs

--- src/domain/categories.spec.ts
+++ src/domain/categories.spec.ts
@@ -8,8 +8,9 @@ export const getCategoryPathTestCases = [
     expected: '/Women/Clothing/Dresses',
   },
   {
+    // It will only match the first Sneakers category it finds, in this case in Women category
     input: { categories: MOCK_CATEGORIES, categoryName: 'Sneakers' },
-    expected: '/Women/Shoes/Sneakers', // First match found (Women branch)
+    expected: '/Women/Shoes/Sneakers',
   },
   {
     input: { categories: MOCK_CATEGORIES, categoryName: 'Boots' },
