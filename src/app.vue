6af36aa2f221d54375beece89d8dc71473bf6a1e
Den <ekisde.xp@gmail.com>
2025-06-21 12:58:01 +0200

    feat(app): improve app ui and css

--- src/app.vue
+++ src/app.vue
@@ -5,24 +5,23 @@ import { CATEGORIES } from '~/domain/constants/categories.const';
 </script>
 
 <template>
-  <div class="Main container">
-    <article>
-      <strong>category4:</strong> {{ getCategoryPath(CATEGORIES, 'category4') }}
-    </article>
-    <article>
-      <strong>category2:</strong> {{ getCategoryPath(CATEGORIES, 'category2') }}
-    </article>
-    <article>
-      <strong>category5:</strong> {{ getCategoryPath(CATEGORIES, 'category5') }}
-    </article>
+  <div class="Main container p-3">
+    <h1>Bershka Technical Test</h1>
+    <div class="w-full flex flex-col">
+      <pre class="p-2">{{ CATEGORIES }}</pre>
+      <h5>Examples:</h5>
+      <article>
+        <strong>category4:</strong> {{ getCategoryPath(CATEGORIES, 'category4') }}
+      </article>
+      <article>
+        <strong>category2:</strong> {{ getCategoryPath(CATEGORIES, 'category2') }}
+      </article>
+      <article>
+        <strong>category5:</strong> {{ getCategoryPath(CATEGORIES, 'category5') }}
+      </article>
+      <article>
+        <strong>category6:</strong> {{ getCategoryPath(CATEGORIES, 'category6') || 'Category not found!' }}
+      </article>
+    </div>
   </div>
 </template>
-
-<style lang="scss">
-  .Main {
-    display: flex;
-    flex-direction: column;
-
-    gap: 12px;
-  }
-</style>
